da4210cb359d86f48126c6cd9c96fedaae2465cf
MariemMezghani <35550711+MariemMezghani@users.noreply.github.com>
2020-04-18 22:39:45 -0300

    Initial commit

---
+++ app/src/main/java/com/github/mariemmezghani/mymoney/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.mariemmezghani.mymoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
